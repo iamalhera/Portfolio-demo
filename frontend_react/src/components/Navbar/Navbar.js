2ff40ef0f1bc6502547f880e1e22b9d87701ae17
Al Hera Ahmad <alheraahmad0786@gmail.com>
2022-08-13 16:46:43 +0530

    added images

--- frontend_react/src/components/Navbar/Navbar.js
+++ frontend_react/src/components/Navbar/Navbar.js
@@ -3,9 +3,17 @@ import './Navbar.scss' ;
 import { images } from '../../constants/index' ;
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="app__navbar">
       <div>
         <img src={images.logo} alt="logo" />
+        <ul>
+          {['home','about','work','skills','contact'].map((item)=>(
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
     )
