3f8d3f5ca2e5afea5087e10f0df237d36b48e5c2
Al Hera Ahmad <alheraahmad0786@gmail.com>
2022-08-23 12:38:09 +0530

    header completed

--- frontend_react/src/container/Header/Header.js
+++ frontend_react/src/container/Header/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
       whiteInView={scaleVariants.whileInView}
       className="app__header-circles"
       >
-      { [images.flutter, images.redux, images.sass, images.react].map((circle,index)=>(
+      { [images.react, images.js, images.sass].map((circle,index)=>(
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="circle" />
         </div>
