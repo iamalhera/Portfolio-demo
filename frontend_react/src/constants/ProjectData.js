ceee70ba51237518a2b83cc44a46dc95c054ac63
Al Hera Ahmad <alheraahmad0786@gmail.com>
2022-08-23 20:59:35 +0530

    about section completed

---
+++ frontend_react/src/constants/ProjectData.js
@@ -0,0 +1,55 @@
+import { images } from '../constants' ;
+import React from 'react';
+const ProjectData = [
+    {
+        title: "APP1",
+        description: "DESCRIPTION1",
+        projectLink: "https://gentleshah.netlify.com",
+        codeLink: "https://github.com",
+        imageURL: images.about01,
+        tag: ["React Apps", "All"]
+    },
+    {
+        title: "APP2",
+        description: "DESCRIPTION3",
+        projectLink: "https://google.com",
+        codeLink: "https://codepen.io",
+        imageURL: images.about02,
+        tag: ["VanillaJS Apps","All"]
+    },
+    {
+        title: "APP3",
+        description: "DESCRIPTION3",
+        projectLink: "https://google.com",
+        codeLink: "https://codepen.io",
+        imageURL: images.about03,
+        tag: ["Websites", "All"]
+    },
+    // {
+    //     title: "APP2",
+    //     description: "DESCRIPTION2",
+    //     imageURL: ""
+    // },
+    // {
+    //     title: "APP3",
+    //     description: "DESCRIPTION3",
+    //     imageURL: ""
+    // },
+    // {
+    //     title: "APP4",
+    //     description: "DESCRIPTION4",
+    //     imageURL: ""
+    // },
+    // {
+    //     title: "APP5",
+    //     description: "DESCRIPTION5",
+    //     imageURL: ""
+    // },
+    // {
+    //     title: "APP5",
+    //     description: "DESCRIPTION5",
+    //     imageURL: ""
+    // }
+];
+
+export default ProjectData;
